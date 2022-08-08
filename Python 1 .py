24fcda3a7aee3fe71cca11d849d8db2b354de567
vidohandojono <110509124+vidohandojono@users.noreply.github.com>
2022-08-08 08:55:11 +0700

    Add files via upload

---
+++ Python 1 .py
@@ -0,0 +1,12 @@
+#output
+
+print("Hello world")   
+print("nama saya Vido")
+print("umur saya 15 tahun")
+print("saya tinggal di Indonesia")
+print("saya bermain game free fire dan mobile legend")
+print("saya mempunyai hobi bermain sepeda")
+print("saya sekolah di SMK HARAPAN BANGSA")
+print("saya pecinta alam dan hewan")
+print("makanan kesukaan saya adalah Nasi padang")
+print("minuman kesukaan saya adalah es choclate")
