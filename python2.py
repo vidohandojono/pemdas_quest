03e07ea03898c19f3f04577c39737538f83fe9e6
vidohandojono <110509124+vidohandojono@users.noreply.github.com>
2022-08-08 08:51:58 +0700

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,11 @@
+
+
+nama = "Vido" # Vido adalah data, nama adalah variabel 
+umur = "15"
+Alamat = "cimanggis, depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("Nama saya adalah " + nama)
