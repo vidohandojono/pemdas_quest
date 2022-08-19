10a76724de8a6e7e6bb5dba1f4d766820604c6a6
vidohandojono <110509124+vidohandojono@users.noreply.github.com>
2022-08-19 02:32:01 -0700

    Add files via upload

---
+++ UPRAK_Vido Handojono_ X TKJ 1.py
@@ -0,0 +1,12 @@
+# Vido handojono Kelas X TKJ 1 
+# Tanggal 19 Agustus 2022
+
+nama = input("masukkan nama anda: ")
+print("nama anda adalah",nama)
+
+kelas = input("masukkan kelas anda: ")
+print("kelas anda adalah",kelas)
+
+tanggal = input("masukkan tanggal hari ini: ")
+print("tanggal hari ini adalah",tanggal)
+
